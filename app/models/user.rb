5dae8cfe91a016c50cb823de1650c1df34abcaa2
yuki fujisawa <fujisawa.yuki@outlook.com>
2021-11-16 11:31:38 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
-end
+  validates :name,  presence: true, length: { maximum: 10 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  end
